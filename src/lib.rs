15b069b8d7c32aa3ba384681a1080db3ddd5c7aa
Tron0xHEX <dm.fl.worker@gmail.com>
2020-07-14 14:05:22 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -18,11 +18,7 @@ use winapi::shared::{
 
 #[no_mangle]
 #[allow(non_snake_case, unused_variables)]
-pub unsafe extern "system" fn _DllMainCRTStartup(
-    dll_module: HINSTANCE,
-    call_reason: DWORD,
-    reserved: LPVOID,
-) -> BOOL {
+pub unsafe extern "system" fn _DllMainCRTStartup(dll_module: HINSTANCE, call_reason: DWORD, reserved: LPVOID) -> BOOL {
     const DLL_PROCESS_ATTACH: DWORD = 1;
     const DLL_PROCESS_DETACH: DWORD = 0;
 
